b6790fec83a0644955e55358a06066830aecca05
Drew Burton <drewburton@users.noreply.github.com>
2020-05-14 16:36:26 -0400

    finished stage 1 of game of life

--- GameOfLife/Game of Life/task/src/life/Main.java
+++ GameOfLife/Game of Life/task/src/life/Main.java
@@ -1,7 +1,30 @@
 package life;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int s = scanner.nextInt();
+
+        generate(n, s);
+    }
+
+    public static void generate(int n, int s) {
+        Random random = new Random(s);
+
+        for (int c = 0; c < n; c++) {
+            for (int r = 0; r < n; r++) {
+                boolean b = random.nextBoolean();
+                if (b) {
+                    System.out.print("O");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
