ffbdd7fb4addef8c26bece2a7d0a9a619458ab8b
Drew Burton <drewburton@users.noreply.github.com>
2020-05-14 15:58:14 -0400

    worked on switch statement practice pt 2

--- Problems/Floor-space of the room/src/Main.java
+++ Problems/Floor-space of the room/src/Main.java
@@ -1,5 +1,69 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-        // put your code here
+        Scanner scanner = new Scanner(System.in);
+        String type = scanner.nextLine();
+
+        double a, b, c, r;
+        switch (type) {
+            case "triangle":
+                a = Double.parseDouble(scanner.nextLine());
+                b = Double.parseDouble(scanner.nextLine());
+                c = Double.parseDouble(scanner.nextLine());
+                System.out.println((new Triangle(a, b, c)).getArea());
+                break;
+            case "rectangle":
+                a = Double.parseDouble(scanner.nextLine());
+                b = Double.parseDouble(scanner.nextLine());
+                System.out.println((new Rectangle(a, b)).getArea());
+                break;
+            case "circle":
+                r = Double.parseDouble(scanner.nextLine());
+                System.out.println((new Circle(r)).getArea());
+                break;
+            default:
+                System.out.println("error!");
+        }
+    }
+}
+
+class Triangle {
+    private double a, b, c;
+
+    public Triangle(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public double getArea() {
+        double p = (a + b + c) / 2;
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    }
+}
+
+class Rectangle {
+    private double a, b;
+
+    public Rectangle(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public double getArea() {
+        return a * b;
+    }
+}
+
+class Circle {
+    private double r;
+
+    public Circle(double r) {
+        this.r = r;
+    }
+
+    public double getArea() {
+        return 3.14 * r * r;
     }
 }
