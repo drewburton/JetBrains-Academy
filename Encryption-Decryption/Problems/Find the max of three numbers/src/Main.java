9c936495d16e6590f28cce0db42959a8f39a1c00
Drew Burton <drewburton@users.noreply.github.com>
2020-05-14 16:38:55 -0400

    deleted extra stuff

--- Encryption-Decryption/Problems/Find the max of three numbers/src/Main.java
+++
@@ -1,9 +0,0 @@
-public static int getNumberOfMaxParam(int a, int b, int c) {
-    if (a >= b && a >= c){
-        return 1;
-    } else if (b >= c) {
-        return 2;
-    } else {
-        return 3;
-    }
-}
