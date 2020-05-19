4c700ccabaa79661b5cdffc6a3d5661f183213be
Drew Burton <drewburton@users.noreply.github.com>
2020-05-19 15:53:29 -0400

    finished stage 2

--- SimpleSearchEngine/Simple Search Engine/task/src/search/Main.java
+++ SimpleSearchEngine/Simple Search Engine/task/src/search/Main.java
@@ -1,20 +1,40 @@
 package search;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String input = scanner.nextLine();
-        String[] words = input.split(" ");
-        String key = scanner.nextLine();
-        for (int wordsIndex = 0; wordsIndex < words.length; wordsIndex++) {
-            if (key.equals(words[wordsIndex])) {
-                System.out.println(wordsIndex + 1);
-                return;
+        int count = scanner.nextInt();
+        scanner.nextLine();
+
+        String[] people = new String[count];
+        for (int i = 0; i < count; i++) {
+            people[i] = scanner.nextLine();
+        }
+
+        int searches = scanner.nextInt();
+        scanner.nextLine();
+
+        for (int i = 0; i < searches; i++) {
+            String data = scanner.nextLine();
+
+            ArrayList<String> foundPeople = new ArrayList<>();
+            for (int peopleIndex = 0; peopleIndex < people.length; peopleIndex++) {
+               if (people[peopleIndex].toLowerCase().matches(".*" + data.toLowerCase() + ".*")) {
+                   foundPeople.add(people[peopleIndex]);
+               }
+            }
+            if (foundPeople.size() > 0) {
+                System.out.println("Found people:");
+                for (int foundIndex = 0; foundIndex < foundPeople.size(); foundIndex++) {
+                    System.out.println(foundPeople.get(foundIndex));
+                }
+            } else {
+                System.out.println("No matching people found.");
             }
         }
-        System.out.println("Not Found");
     }
 }
