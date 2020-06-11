33c106ef056b98e187ad2add534f10cc256ff796
Drew Burton <drewburton@users.noreply.github.com>
2020-06-11 12:43:54 -0400

    added complex number class and implemented math functions

--- Linear Equations Solver/Linear Equations Solver/task/src/solver/Complex.java
+++ Linear Equations Solver/Linear Equations Solver/task/src/solver/Complex.java
@@ -6,15 +6,104 @@ import java.util.regex.Pattern;
 public class Complex {
     private double real;
     private double imaginary;
-    private boolean conjugate;
 
     public Complex(double real, double imaginary) {
         this.real = real;
-        this.imaginary = Math.abs(imaginary);
-        conjugate = imaginary < 0 ? true : false;
+        this.imaginary = imaginary;
     }
 
     public Complex(String number) {
-        
+        Pattern plusPattern = Pattern.compile("\\+");
+        Pattern minusPattern = Pattern.compile("-");
+        if (plusPattern.matcher(number).find()) {
+            String[] parts = number.split(plusPattern.toString());
+            real = Integer.parseInt(parts[0]);
+            try {
+                imaginary = Integer.parseInt(parts[1].substring(0, parts[1].length() - 1));
+            } catch (NumberFormatException e) {
+                imaginary = 1;
+            }
+        } else if (minusPattern.matcher(number.substring(1)).find()) {
+            String[] parts = number.split(minusPattern.toString());
+
+            if (number.charAt(0) == '-') {
+                try {
+                    real = -Integer.parseInt(parts[1]);
+                    imaginary = -Integer.parseInt(parts[2].substring(0, parts[2].length() - 1));
+                } catch (NumberFormatException e) {
+                    imaginary = -1;
+                }
+            } else {
+                try {
+                    real = Integer.parseInt(parts[0]);
+                    imaginary = -Integer.parseInt(parts[1].substring(0, parts[1].length() - 1));
+                } catch (NumberFormatException e) {
+                    imaginary = 1;
+                }
+            }
+        } else {
+            real = number.contains("i") ? 0 : Integer.parseInt(number);
+            try {
+                imaginary = number.contains("i") ? Integer.parseInt(number.substring(0, number.length() - 1)) : 0;
+            } catch (NumberFormatException e) {
+                if (number.contains("-")) {
+                   imaginary = -1;
+                } else {
+                    imaginary = 1;
+                }
+            }
+        }
+    }
+
+    public Complex add(Complex num) {
+        double real = this.real + num.real;
+        double imaginary = this.imaginary + num.imaginary;
+
+        return new Complex(real, imaginary);
+    }
+
+    public Complex subtract(Complex num) {
+        double real = this.real - num.real;
+        double imaginary = this.imaginary - num.imaginary;
+
+        return new Complex(real, imaginary);
+    }
+
+    public Complex multiply(Complex num) {
+        double real = this.real * num.real;
+        double imaginary = this.imaginary * num.imaginary;
+
+        return new Complex(real, imaginary);
+    }
+
+    public Complex divide(Complex num) throws ArithmeticException {
+        double real = this.real / num.real;
+        double imaginary = this.imaginary / num.imaginary;
+
+        return new Complex(real, imaginary);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder complexNum = new StringBuilder();
+        if (real != 0) {
+            complexNum.append(real);
+        }
+
+        if (imaginary == 1 && real != 0) {
+            complexNum.append("+i");
+        } else if (imaginary == -1) {
+            complexNum.append("-i");
+        } else if (imaginary == 1) {
+            complexNum.append("i");
+        } else if (imaginary != 0 && imaginary < 0) {
+            complexNum.append(imaginary + "i");
+        } else if (imaginary != 0 && complexNum.length() > 0){
+            complexNum.append("+" + imaginary + "i");
+        } else if (imaginary != 0) {
+            complexNum.append(imaginary + "i");
+        }
+
+        return complexNum.toString();
     }
 }
--- Linear Equations Solver/Linear Equations Solver/task/src/solver/Main.java
+++ Linear Equations Solver/Linear Equations Solver/task/src/solver/Main.java
@@ -6,21 +6,35 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        LinearEquation equation = new LinearEquation(args);
-        ArrayList<String> answers = equation.solve();
+        Complex number = new Complex(1, 2);
+        Complex number2 = new Complex("1+i");
+        Complex number3 = new Complex("1-1i");
+        Complex number4 = new Complex("-1-i");
+        Complex number5 = new Complex("1");
+        Complex number6 = new Complex("-i");
 
-        try {
-            File file = new File(args[3]);
-            file.createNewFile();
-            System.out.println("created: " + file.toString());
-            FileWriter writer = new FileWriter(args[3]);
+//        System.out.println(number.toString());
+//        System.out.println(number2.toString());
+//        System.out.println(number3.toString());
+//        System.out.println(number4.toString());
+//        System.out.println(number5.toString());
+//        System.out.println(number6.toString());
 
-            System.out.println("Writing to file:");
-            for (String answer : answers) {
-                System.out.println(answer);
-                writer.write(answer + "\n");
-            }
-            writer.close();
-        } catch (Exception e) {}
+//        LinearEquation equation = new LinearEquation(args);
+//        ArrayList<String> answers = equation.solve();
+//
+//        try {
+//            File file = new File(args[3]);
+//            file.createNewFile();
+//            System.out.println("created: " + file.toString());
+//            FileWriter writer = new FileWriter(args[3]);
+//
+//            System.out.println("Writing to file:");
+//            for (String answer : answers) {
+//                System.out.println(answer);
+//                writer.write(answer + "\n");
+//            }
+//            writer.close();
+//        } catch (Exception e) {}
     }
 }
