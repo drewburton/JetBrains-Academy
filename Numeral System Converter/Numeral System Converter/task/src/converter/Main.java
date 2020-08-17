e3d23f624b8077fbb235e2a5a959de5f93365c49
Drew Burton <drewburton@users.noreply.github.com>
2020-08-17 15:17:09 -0400

    finished stage 4 of numeral system converter

--- Numeral System Converter/Numeral System Converter/task/src/converter/Main.java
+++ Numeral System Converter/Numeral System Converter/task/src/converter/Main.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int decimal = Integer.parseInt(scanner.nextLine());
-        NumeralSystem decimalNumber = new Decimal(decimal);
-
         int radix = Integer.parseInt(scanner.nextLine());
-        System.out.println(Converter.convert(radix, decimalNumber));
+        NumeralSystem number = new NumeralSystem(scanner.nextLine(), radix);
+        int newRadix = Integer.parseInt(scanner.nextLine());
+        number.convert(newRadix);
+        System.out.println(number);
     }
 }
--- Numeral System Converter/Numeral System Converter/task/src/converter/NumeralSystem.java
+++ Numeral System Converter/Numeral System Converter/task/src/converter/NumeralSystem.java
@@ -1,18 +1,32 @@
 package converter;
 
-public abstract class NumeralSystem {
-    protected int number;
+public class NumeralSystem {
+    private int number;
+    private int radix;
 
-    public NumeralSystem(String number) {
-        this.number = toDecimal(number);
+
+    public NumeralSystem(String number, int radix) {
+        if (radix == 1) {
+            this.number = number.length();
+        } else {
+            this.number = Integer.parseInt(number, radix);
+        }
+        this.radix = radix;
     }
 
-    public NumeralSystem(int number) {
-        this.number = number;
+    public void convert(int newRadix) {
+        radix = newRadix;
     }
-
-    protected abstract int toDecimal(String number);
 
     @Override
-    public abstract String toString();
+    public String toString() {
+        if (radix == 1) {
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < number; i++) {
+                builder.append("1");
+            }
+            return builder.toString();
+        }
+        return Integer.toString(number, radix);
+    }
 }
