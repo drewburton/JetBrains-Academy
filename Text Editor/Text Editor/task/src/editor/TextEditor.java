2e1c7e6dececa57d27da78f0dbb2b186d81d2318
Drew Burton <drewburton@users.noreply.github.com>
2020-08-29 15:04:55 -0400

    started text editor project and finished stage 1

--- Text Editor/Text Editor/task/src/editor/TextEditor.java
+++ Text Editor/Text Editor/task/src/editor/TextEditor.java
@@ -1,12 +1,26 @@
 package editor;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
 
 public class TextEditor extends JFrame {
+    JTextArea textArea;
+
     public TextEditor() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(300, 300);
         setVisible(true);
         setLayout(null);
+        setTitle("Text Editor");
+
+        textArea = new JTextArea();
+        textArea.setName("TextArea");
+        textArea.setSize(250, 250);
+        textArea.setLineWrap(true);
+        textArea.setVisible(true);
+        getContentPane().add(textArea);
+
+        validate();
     }
 }
